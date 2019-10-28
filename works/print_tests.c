d445f70471a9bb225dcdb98124bd2bd96df80569
user <khuzamaywbf@hotmail.com>
2019-10-28 14:14:31 +0200

    20191028_14_15

---
+++ works/print_tests.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main()
+ {
+ float a=1.23456, b=7.89012, c=34.5678;
+ printf(...
+ // wrtite code to show:
+/*
+cmd:>gcc printf_tests.c
+cmd:>./a.out
+1.2      7.89012 34.5678
+1.23     7.8901  34.56780
+1.23456  7.890   34.567800 
+*/
+ // Rule - don't use (put) spaces " "!!!
+}
+
+
