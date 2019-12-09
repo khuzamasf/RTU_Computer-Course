8920cbfe3ae3067665c63686f49f85b8b14a4519
user <khuzamaywbf@hotmail.com>
2019-12-09 13:42:10 +0200

    20191208

---
+++ works/work-with-files/fprintf_example.c
@@ -0,0 +1,21 @@
+/* fprintf example */
+#include <stdio.h>
+
+int main ()
+{
+   FILE * pFile;
+   int n;
+   char name [100];
+
+   pFile = fopen ("myfile.txt","w");
+   for (n=0 ; n<3 ; n++)
+   {
+    //puts
+    printf("please, enter a name: ");
+    //gets (name);
+     fprintf (pFile, "Name %d [%-10.10s]\n",n+1,name);
+   }
+   fclose (pFile);
+
+   return 0;
+}
